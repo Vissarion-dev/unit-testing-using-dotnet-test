a6daa72b013cebc00cc086a146fbfe0274b6fc36
Vissarion <vissarion@deryabin.info>
2022-10-01 19:14:08 +0300

    first commit

---
+++ PrimeService.Tests/PriceCalculator_Tests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using Prime.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Prime.Services.PriceCalculator;
+
+namespace Prime.UnitTests.Calculator
+{
+    public class PriceCalculator_Tests
+    {
+        [Fact]
+        public void GetDiscountedPrice_NotTuesday_ReturnsFullPrice()
+        {
+            var priceCalculator = new PriceCalculator();
+            var dateTimeProviderStub = new Mock<IDateTimeProvider>();
+            dateTimeProviderStub.Setup(dtp => dtp.DayOfWeek()).Returns(DayOfWeek.Monday);
+
+            var actual = priceCalculator.GetDiscountedPrice(2, dateTimeProviderStub.Object);
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void GetDiscountedPrice_OnTuesday_ReturnsHalfPrice()
+        {
+            var priceCalculator = new PriceCalculator();
+            var dateTimeProviderStub = new Mock<IDateTimeProvider>();
+            dateTimeProviderStub.Setup(dtp => dtp.DayOfWeek()).Returns(DayOfWeek.Tuesday);
+
+            var actual = priceCalculator.GetDiscountedPrice(2, dateTimeProviderStub.Object);
+
+            Assert.Equal(1, actual);
+        }
+    }
+}
---
+++ PrimeService/PriceCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime.Services
+{
+    public class PriceCalculator
+    {
+        public interface IDateTimeProvider
+        {
+            DayOfWeek DayOfWeek();
+        }
+        public int GetDiscountedPrice(int price, IDateTimeProvider dateTimeProvider)
+        {
+            if (dateTimeProvider.DayOfWeek() == DayOfWeek.Tuesday)
+            {
+                return price / 2;
+            }
+            else
+            {
+                return price;
+            }
+        }
+    }
+}
